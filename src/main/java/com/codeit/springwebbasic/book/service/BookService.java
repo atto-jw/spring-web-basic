13e18f5b421f8ce142a72a9aa734b3c4ff9972e2
atto-jw <atto.jw@gmail.com>
2025-10-21 18:05:47 +0900

    도서 단일 조회 로직 작성, 전역 예외 처리 클래스 작성

--- src/main/java/com/codeit/springwebbasic/book/service/BookService.java
+++ src/main/java/com/codeit/springwebbasic/book/service/BookService.java
@@ -1,39 +1,44 @@
-package com.codeit.springwebbasic.book.service;
+    package com.codeit.springwebbasic.book.service;
 
-import com.codeit.springwebbasic.book.dto.request.BookCreateRequestDto;
-import com.codeit.springwebbasic.book.entity.Book;
-import com.codeit.springwebbasic.book.entity.BookStatus;
-import com.codeit.springwebbasic.book.repository.BookRepository;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+    import com.codeit.springwebbasic.book.dto.request.BookCreateRequestDto;
+    import com.codeit.springwebbasic.book.entity.Book;
+    import com.codeit.springwebbasic.book.entity.BookStatus;
+    import com.codeit.springwebbasic.book.repository.BookRepository;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-public class BookService {
+    @Service
+    @RequiredArgsConstructor
+    public class BookService {
 
-    private final BookRepository bookRepository;
+        private final BookRepository bookRepository;
 
-    public Book createBook(BookCreateRequestDto requestDto) {
-        //ISBN 중복 체크
-        Optional<Book> byIsbn = bookRepository.findByIsbn(requestDto.getIsbn());
-        if (byIsbn.isPresent()){ //Book 객체가 존재한지?
-            throw new IllegalArgumentException("이미 등록된 ISBN입니다. : " + requestDto.getIsbn());
+        public Book createBook(BookCreateRequestDto requestDto) {
+            //ISBN 중복 체크
+            Optional<Book> byIsbn = bookRepository.findByIsbn(requestDto.getIsbn());
+            if (byIsbn.isPresent()){ //Book 객체가 존재한지?
+                throw new IllegalArgumentException("이미 등록된 ISBN입니다. : " + requestDto.getIsbn());
+            }
+            //Builder 패턴(직관적으로 객체 생성 가능)
+            //이 패턴을 활용하면 초기화 순서를 내 마음대로 지정가능, 원하는 필드만 골라서 초기화 하는것이 가능.
+            Book book = Book.builder()
+                    .title(requestDto.getTitle())
+                    .isbn(requestDto.getIsbn())
+                    .author(requestDto.getAuthor())
+                    .publisher(requestDto.getPublisher())
+                    .publishDate(requestDto.getPublicationDate())
+                    .status(BookStatus.AVAILABLE)
+                    .build();
+
+            return bookRepository.save(book);
+
         }
-        //Builder 패턴(직관적으로 객체 생성 가능)
-        //이 패턴을 활용하면 초기화 순서를 내 마음대로 지정가능, 원하는 필드만 골라서 초기화 하는것이 가능.
-        Book book = Book.builder()
-                .title(requestDto.getTitle())
-                .isbn(requestDto.getIsbn())
-                .author(requestDto.getAuthor())
-                .publisher(requestDto.getPublisher())
-                .publishDate(requestDto.getPublicationDate())
-                .status(BookStatus.AVAILABLE)
-                .build();
 
-        return bookRepository.save(book);
 
+        public Book getBook(Long id) {
+            return bookRepository.findById(id) //책이 찾았는데 없다면 예외 발생
+                    .orElseThrow(() -> new IllegalArgumentException("도서를 찾을 수 없습니다."));
+        }
     }
-}
