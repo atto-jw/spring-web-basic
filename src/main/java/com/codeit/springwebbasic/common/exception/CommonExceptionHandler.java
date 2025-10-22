28318ee8e75eea5fdd7c33d7187787684c767f21
atto-jw <atto.jw@gmail.com>
2025-10-22 11:51:21 +0900

    Validation 관련 예외 처리 방법

--- src/main/java/com/codeit/springwebbasic/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/codeit/springwebbasic/common/exception/CommonExceptionHandler.java
@@ -2,8 +2,15 @@ package com.codeit.springwebbasic.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
@@ -25,6 +32,38 @@ public class CommonExceptionHandler {
         e.printStackTrace();
         // 예외의 원인을 http 상태 코드와 메세지를 통해서 알려주고 싶다 -> ResponseEntity
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidHandler(MethodArgumentNotValidException e){
+        e.printStackTrace();
+
+        // 1. 오류 결과를 담을 Map을 생성 (Key: 필드명, Value: 에러 메시지)
+        Map<String, String> errors =  new HashMap<>();
+
+        /*
+        // BindingResult : 오류 결과 보고서
+        BindingResult bindingResult = e.getBindingResult();
+
+        // BindingResult에서 @Valid에 실패한 필드 목록을 불러옵니다.
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError error : fieldErrors) {
+            String field = error.getField();
+            String Message = error.getDefaultMessage();
+            errors.put(field, Message);
+        }
+         */
+
+        // 간추려서 사용하기
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String Message = error.getDefaultMessage();
+            errors.put(field, Message);
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 
     // 미처 준비하지 못한 타입의 예외가 발생했을 시 처리할 메서드
